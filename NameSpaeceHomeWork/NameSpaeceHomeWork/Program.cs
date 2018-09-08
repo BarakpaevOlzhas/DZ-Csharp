d6ec5793068e434701a06c4b056c4c06234887e0
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2018-09-08 22:54:40 +0600

    Add files via upload

---
+++ NameSpaeceHomeWork/NameSpaeceHomeWork/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypePerson;
+
+namespace NameSpaeceHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.FirstName = "Олжас";
+
+            person.MiddleName = "Баракпаев";
+
+            person.LastName = "Булатович";
+
+            person.Age = 15;
+
+            Console.WriteLine(Show.ShowAllStats(person));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NameSpaeceHomeWork/TypePerson/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypePerson
+{
+    public class Person
+    {
+        public string FirstName { set; get; }
+
+        public string MiddleName { set; get; }
+
+        public string LastName { set; get; }
+
+        public int Age { set; get; }
+    }
+
+    static public class Show
+    {
+        static public string ShowAllStats(Person person)
+        {
+            return string.Format(person.MiddleName + " " + person.FirstName + " " + person.LastName + " " + person.Age);
+        }
+
+    }
+}
