bf478e94ffad74a75d7b225e3a95c0e363c55148
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2018-09-11 18:06:29 +0600

    Add files via upload

---
+++ Dz_ciSharp/IndexatorsDZ/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MMArray;
+
+namespace IndexatorsDZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            int elements;
+            int index;
+
+            MArray mArray = new MArray();
+
+            Console.WriteLine("Введите кол-во элементов:");
+
+            int.TryParse(Console.ReadLine(),out size);
+
+            mArray.Array = new int[size];
+
+            Console.WriteLine("Введите элементы массива");
+
+            for (int i = 0;i < size;i++)
+            {
+                int.TryParse(Console.ReadLine(), out elements);
+                mArray[i] = elements;
+            }
+
+            Console.WriteLine("Элементы");
+
+            for (int i = 0;i < size;i++)
+            {
+                Console.Write(mArray[i] + " ");
+            }
+
+            Console.WriteLine("\nВведите индекс");
+
+            int.TryParse(Console.ReadLine(), out index);
+            
+            Console.WriteLine(mArray.ReturnToIndex(index));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dz_ciSharp/MArray/MArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMArray
+{
+    public class MArray
+    {
+        public int[] Array { get; set; }
+        
+        public int this[int index]
+        {
+            get
+            {
+                return Array[index];
+            }
+            set
+            {
+                Array[index] = value * value;
+            }
+        }
+
+        public int ReturnToIndex(int index)
+        {
+            return Array[index];
+        }
+
+    }
+}
