45ebd8abbb7eea41a5c5c5d0a5c18f4b03726b8e
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2018-09-29 20:51:08 +0600

    Add files via upload

---
+++ Day7(Tanks)/Day7(Tanks)/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib;
+using System.Threading;
+
+namespace Day7_Tanks_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int five = 5;
+
+            Tank[] tanksOne = new Tank[five];
+            Tank[] tanksTwo = new Tank[five];
+
+            for (int i = 0; i < five; i++) 
+            {
+                Thread.Sleep(10);
+
+                tanksOne[i] = new Tank("Т-34");
+
+                Thread.Sleep(10);
+
+                tanksTwo[i] = new Tank("Pantera");
+            }
+
+            int pointTeamOne = 0;
+            int pointTeamTwo = 0;
+
+            for (int i = 0; i < five; i++)
+            {
+                int winner = 0;
+
+                Console.Clear();
+                
+                Console.WriteLine(tanksOne[i].GetStats());
+
+                Console.WriteLine();
+
+                Console.WriteLine(tanksTwo[i].GetStats());
+
+                winner = tanksOne[i] * tanksTwo[i];
+
+                if (winner == 1)
+                {
+                    Console.WriteLine("T-34 Win");
+                    pointTeamOne++;
+                }
+
+                else if (winner == 0)
+                {
+                    Console.WriteLine("Pantera Win");
+                    pointTeamTwo++;
+                }
+
+                else if (winner == -1)
+                {
+                    Console.WriteLine("Ничья");
+                }
+
+                Console.ReadLine();
+            }
+
+            if (pointTeamOne > pointTeamTwo)
+            {
+                Console.WriteLine("Команда Т-34 победила");
+            }
+
+           else if (pointTeamOne < pointTeamTwo)
+            {
+                Console.WriteLine("Команда Pantera победила");
+            }
+
+            else
+            {
+                Console.WriteLine("Ничья");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day7(Tanks)/MyClassLib/Tank.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+    public class Tank
+    {
+        public string Name { get; set; }
+
+        public int LevelOfAmmunition { get; set; }
+
+        public int LevelOfArmor { get; set; }
+
+        public int LevelOfManeuverability { get; set; }
+
+        public Tank(string name)
+        {
+            Random random = new Random();
+            
+            Name = name;
+
+            LevelOfAmmunition = random.Next(0,100);
+
+            LevelOfArmor = random.Next(0, 100);
+
+            LevelOfManeuverability = random.Next(0, 100);
+        }
+
+        public string GetStats()
+        {
+            return $"Название {Name} \n Уровня боекомплекта танка: {LevelOfAmmunition} \n Уровня брони: {LevelOfArmor} \n Уровня маневренности: {LevelOfManeuverability}";
+        }
+
+        public static int operator *(Tank tankOne, Tank tankTwo)
+        {
+            int plusOne = 0;
+            int plusTwo = 0;
+
+            if (tankOne.LevelOfAmmunition > tankTwo.LevelOfAmmunition)
+            {
+                plusOne++;
+            }
+
+            else if (tankOne.LevelOfAmmunition < tankTwo.LevelOfAmmunition)
+            {
+                plusTwo++;
+            }
+
+            if (tankOne.LevelOfArmor > tankTwo.LevelOfArmor)
+            {
+                plusOne++;
+            }
+
+            else if (tankOne.LevelOfArmor < tankTwo.LevelOfArmor)
+            {
+                plusTwo++;
+            }
+
+            if (tankOne.LevelOfManeuverability > tankTwo.LevelOfManeuverability)
+            {
+                plusOne++;
+            }
+
+            else if (tankOne.LevelOfManeuverability < tankTwo.LevelOfManeuverability)
+            {
+                plusTwo++;
+            }
+
+            if (plusOne > plusTwo)
+            {
+                return 1;
+            }
+
+            else if (plusOne < plusTwo)
+            {
+                return 0;
+            }
+
+            return -1;
+        }
+
+    }
+}
